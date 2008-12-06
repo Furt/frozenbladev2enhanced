061892543a7dc43fb7115053190a88a11179dd04
furt82 <furt82@e3bcf03a-c382-11dd-ae62-6dab0a2b1684>
2008-12-06 11:26:30 +0000

    Added FrozenBlade v2 by kitten. For Arcemu only atm.

---
+++ ArcEmu/staff.php
@@ -0,0 +1,497 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+
+<title>FrozenBlade Template by Kitten @ WoWps.org</title>
+
+<link rel="shortcut icon" href="images/favicon.ico">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<!--[if IE]><link href="css/ie-fix.css" rel="stylesheet" type="text/css"><![endif]-->
+<script type="text/javascript" src="./js/img-trans.js"></script>
+<script type="text/javascript" src="./js/pre-load.js"></script>
+</head><body>
+
+<div class="maintile"><div class="blue"><div class="gryphon-right"><div class="gryphon-left"></div></div></div></div><div class="wowlogo"></div><div></div><div class="container"><div class="top"></div>
+
+
+<div class="banner"></div>
+
+
+<div class="bar"></div><div class="inner2"><table align="center" width="718" border="0" cellspacing="1" cellpadding="1"><tr><td width="144" valign="top"><div id="links"><font class="style10">
+
+<div class="menu"></div>
+	
+	
+		<!-- Menu Links -->
+	
+				<li><a href="home.php">Home</a></li><br/><br/>
+				<li><a href="donate.php">Make a Donation</a></li><br/><br/>
+				<li><a href="connect.php">Connection Guide</a></li><br/><br/>
+				<li><a href="staff.php">Staff Members</a></li><br/><br/>
+	
+		<!-- --------- -->
+		
+			
+<div class="account"></div>
+	 
+		
+		<!-- Account Links-->
+			
+				<li><a href="#">Create Account</a></li><br/><br/>
+				<li><a href="#">Change Password</a></li><br/><br/>
+				<li><a href="#">Get Password</a></li><br/><br/>
+				<li><a href="#">Ban Checker</a></li><br/><br/>
+				<li><a href="#">IP Ban Checker</a></li><br/><br/>
+		
+		<!-- --------- -->		
+		
+			
+<div class="workshop"></div>
+				
+		
+		<!-- Workshop Links -->
+			
+				<li><a href="#">Vote Page</a></li><br/><br/>
+				<li><a href="#">Vote Panel</a></li><br/><br/>
+				<li><a href="#">Unstucker</a></li><br/><br/>
+				<li><a href="#">Teleporter</a></li><br/><br/>
+				<li><a href="#">Status Page</a></li><br/><br/>
+				<li><a href="#">Honor Page</a></li><br/><br/>
+		
+		<!-- --------- -->
+			
+			
+<div class="forum"></div>
+			
+
+		<!-- Forum Links -->
+			
+				<li><a href="#">Forum Home</a></li><br/><br/>
+				<li><a href="#">Bug Reporting</a></li><br/><br/>
+				<li><a href="#">Announcements</a></li><br/><br/>
+				<li><a href="#">General Chat</a></li><br/><br/>
+				<li><a href="#">General Support</a></li><br/><br/>
+				
+		<!-- --------- -->	
+	
+	
+</font></div><br><a href="http://wowps.org/forum"><div class="by"></div></a></td><td width="574" valign="top">
+
+
+
+		<!-- Begin Administrators Section --><!-- Begin Administrators Section -->
+		<!-- Begin Administrators Section --><!-- Begin Administrators Section -->
+		<!-- Begin Administrators Section --><!-- Begin Administrators Section -->
+			
+<div class="story-top6">
+<div align="center" style="text-align:bottom"><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+
+	<!-- Header Text Image -->
+			<img src="images/text/admin.png">	
+
+</div></div><div class="story2"><center><div style="width:400px; text-align:left"><center><br/><br/>
+
+
+
+				<!-- Start Staff Member --><!-- Start Staff Member -->
+				<!-- Start Staff Member --><!-- Start Staff Member -->
+
+<div class="bg1"><div class="bg2"><table width="100%" height="100%" border="0" cellspacing="0" cellpadding="0"><tr><td width="140" height="100%" valign="top"><div class="box1"><div class="charbg">
+
+
+	<!-- character details -->
+			<!-- please refer to the guide to change character details -->
+	
+			<div class="nightelf-female"></div>
+			<div class="hunter"></div>
+			<div class="alliance"></div>
+			
+</div><div class="userpic">
+
+	<!-- user image -->
+
+				<img src="images/user/47.gif" />
+
+</div><div class="user">
+
+	<!-- name and position -->
+	
+			<b>Kitten</b><br/>			Web Designer
+	   
+</div></div>
+
+</div></td><td height="100%" valign="top"><div class="box2"><br>
+
+	<!-- general information -->
+			
+&nbsp;			Name: 				??				<br/>
+&nbsp;			Location: 			??				<br/>
+&nbsp;			Age: 				??				<br/><br/>
+
+
+&nbsp;			Information: 		??				</div></td></tr></table></div></div><br/>
+
+
+				<!-- End Staff Member --><!-- End Staff Member -->
+				<!-- End Staff Member --><!-- End Staff Member -->
+
+
+				<!-- Start Staff Member --><!-- Start Staff Member -->
+				<!-- Start Staff Member --><!-- Start Staff Member -->
+
+<div class="bg1"><div class="bg2"><table width="100%" height="100%" border="0" cellspacing="0" cellpadding="0"><tr><td width="140" height="100%" valign="top"><div class="box1"><div class="charbg">
+
+
+	<!-- character details -->
+			<!-- please refer to the guide to change character details -->
+	
+			<div class="bloodelf-male"></div>
+			<div class="paladin"></div>
+			<div class="horde"></div>
+			
+</div><div class="userpic">
+
+	<!-- user image -->
+
+				<img src="images/user/19.gif">
+
+</div><div class="user">
+
+	<!-- name and position -->
+	
+			<b>DJRavine</b><br/>			PHP Coder
+	   
+</div></div>
+
+</div></td><td height="100%" valign="top"><div class="box2"><br>
+
+	<!-- general information -->
+			
+&nbsp;			Name: 				??				<br/>
+&nbsp;			Location: 			??				<br/>
+&nbsp;			Age: 				??				<br/><br/>
+
+
+&nbsp;			Information: 		??				</div></td></tr></table></div></div><br/>
+
+
+				<!-- End Staff Member --><!-- End Staff Member -->
+				<!-- End Staff Member --><!-- End Staff Member -->
+
+						
+</div></center></div><div class="story-bot2"></div>
+
+		<!-- End Administrators Section --><!-- End Administrators Section -->
+		<!-- End Administrators Section --><!-- End Administrators Section -->
+		<!-- End Administrators Section --><!-- End Administrators Section -->
+
+
+
+		<!-- Begin Moderators Section --><!-- Begin Moderators Section -->
+		<!-- Begin Moderators Section --><!-- Begin Moderators Section -->
+		<!-- Begin Moderators Section --><!-- Begin Moderators Section -->
+			
+<div class="story-top6">
+<div align="center" style="text-align:bottom"><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+
+	<!-- Header Text Image -->
+			<img src="images/text/mod.png">	
+			
+</div></div><div class="story2"><center><div style="width:400px; text-align:left"><center><br/><br/>
+
+
+
+				<!-- Start Staff Member --><!-- Start Staff Member -->
+				<!-- Start Staff Member --><!-- Start Staff Member -->
+
+<div class="bg1"><div class="bg2"><table width="100%" height="100%" border="0" cellspacing="0" cellpadding="0"><tr><td width="140" height="100%" valign="top"><div class="box1"><div class="charbg">
+
+
+	<!-- character details -->
+			<!-- please refer to the guide to change character details -->
+	
+			<div class="human-male"></div>
+			<div class="warrior"></div>
+			<div class="alliance"></div>
+			
+</div><div class="userpic">
+
+	<!-- user image -->
+			<!-- simply remove the code before <img src="image name"> and after for the image to be displayed -->
+
+				<!--<img src="images/user/47.gif" />-->
+				
+</div><div class="user">
+
+	<!-- name and position -->
+	
+			<b>Username</b><br/>			Position
+
+	   
+</div></div>
+
+</div></td><td height="100%" valign="top"><div class="box2"><br>
+
+	<!-- general information -->
+			
+&nbsp;			Name: 				??				<br/>
+&nbsp;			Location: 			??				<br/>
+&nbsp;			Age: 				??				<br/><br/>
+
+
+&nbsp;			Information: 		??				</div></td></tr></table></div></div><br/>
+
+
+				<!-- End Staff Member --><!-- End Staff Member -->
+				<!-- End Staff Member --><!-- End Staff Member -->
+
+				<!-- Start Staff Member --><!-- Start Staff Member -->
+				<!-- Start Staff Member --><!-- Start Staff Member -->
+
+<div class="bg1"><div class="bg2"><table width="100%" height="100%" border="0" cellspacing="0" cellpadding="0"><tr><td width="140" height="100%" valign="top"><div class="box1"><div class="charbg">
+
+
+	<!-- character details -->
+			<!-- please refer to the guide to change character details -->
+	
+			<div class="human-male"></div>
+			<div class="warrior"></div>
+			<div class="alliance"></div>
+			
+</div><div class="userpic">
+
+	<!-- user image -->
+			<!-- simply remove the code before <img src="image name"> and after for the image to be displayed -->
+
+				<!--<img src="images/user/47.gif" />-->
+
+</div><div class="user">
+
+	<!-- name and position -->
+	
+			<b>Username</b><br/>			Position
+
+	   
+</div></div>
+
+</div></td><td height="100%" valign="top"><div class="box2"><br>
+
+	<!-- general information -->
+			
+&nbsp;			Name: 				??				<br/>
+&nbsp;			Location: 			??				<br/>
+&nbsp;			Age: 				??				<br/><br/>
+
+
+&nbsp;			Information: 		??				</div></td></tr></table></div></div><br/>
+
+
+				<!-- End Staff Member --><!-- End Staff Member -->
+				<!-- End Staff Member --><!-- End Staff Member -->
+	
+</div></center></div><div class="story-bot2"></div>
+
+		<!-- End Moderators Section --><!-- End Moderators Section -->
+		<!-- End Moderators Section --><!-- End Moderators Section -->
+		<!-- End Moderators Section --><!-- End Moderators Section -->
+
+
+
+		<!-- Begin Game Masters Section --><!-- Begin Game Masters Section -->
+		<!-- Begin Game Masters Section --><!-- Begin Game Masters Section -->
+		<!-- Begin Game Masters Section --><!-- Begin Game Masters Section -->
+			
+<div class="story-top6">
+<div align="center" style="text-align:bottom"><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+
+	<!-- Header Text Image -->
+			<img src="images/text/gm.png">	
+
+</div></div><div class="story2"><center><div style="width:400px; text-align:left"><center><br/><br/>
+
+
+
+				<!-- Start Staff Member --><!-- Start Staff Member -->
+				<!-- Start Staff Member --><!-- Start Staff Member -->
+
+<div class="bg1"><div class="bg2"><table width="100%" height="100%" border="0" cellspacing="0" cellpadding="0"><tr><td width="140" height="100%" valign="top"><div class="box1"><div class="charbg">
+
+
+	<!-- character details -->
+			<!-- please refer to the guide to change character details -->
+	
+			<div class="human-male"></div>
+			<div class="warrior"></div>
+			<div class="alliance"></div>
+			
+</div><div class="userpic">
+
+	<!-- user image -->
+			<!-- simply remove the code before <img src="image name"> and after for the image to be displayed -->
+
+				<!--<img src="images/user/47.gif" />-->
+				
+</div><div class="user">
+
+	<!-- name and position -->
+	
+			<b>Username</b><br/>			Position
+	   
+</div></div>
+
+</div></td><td height="100%" valign="top"><div class="box2"><br>
+
+	<!-- general information -->
+			
+&nbsp;			Name: 				??				<br/>
+&nbsp;			Location: 			??				<br/>
+&nbsp;			Age: 				??				<br/><br/>
+
+
+&nbsp;			Information: 		??				</div></td></tr></table></div></div><br/>
+
+
+				<!-- End Staff Member --><!-- End Staff Member -->
+				<!-- End Staff Member --><!-- End Staff Member -->
+
+
+				<!-- Start Staff Member --><!-- Start Staff Member -->
+				<!-- Start Staff Member --><!-- Start Staff Member -->
+
+<div class="bg1"><div class="bg2"><table width="100%" height="100%" border="0" cellspacing="0" cellpadding="0"><tr><td width="140" height="100%" valign="top"><div class="box1"><div class="charbg">
+
+
+	<!-- character details -->
+			<!-- please refer to the guide to change character details -->
+	
+			<div class="human-male"></div>
+			<div class="warrior"></div>
+			<div class="alliance"></div>
+			
+</div><div class="userpic">
+
+	<!-- user image -->
+			<!-- simply remove the code before <img src="image name"> and after for the image to be displayed -->
+
+				<!--<img src="images/user/47.gif" />-->
+
+</div><div class="user">
+
+	<!-- name and position -->
+	
+			<b>Username</b><br/>			Position
+	   
+</div></div>
+
+</div></td><td height="100%" valign="top"><div class="box2"><br>
+
+	<!-- general information -->
+			
+&nbsp;			Name: 				??				<br/>
+&nbsp;			Location: 			??				<br/>
+&nbsp;			Age: 				??				<br/><br/>
+
+
+&nbsp;			Information: 		??				</div></td></tr></table></div></div><br/>
+
+
+				<!-- End Staff Member --><!-- End Staff Member -->
+				<!-- End Staff Member --><!-- End Staff Member -->
+
+
+				<!-- Start Staff Member --><!-- Start Staff Member -->
+				<!-- Start Staff Member --><!-- Start Staff Member -->
+
+<div class="bg1"><div class="bg2"><table width="100%" height="100%" border="0" cellspacing="0" cellpadding="0"><tr><td width="140" height="100%" valign="top"><div class="box1"><div class="charbg">
+
+
+	<!-- character details -->
+			<!-- please refer to the guide to change character details -->
+	
+			<div class="human-male"></div>
+			<div class="warrior"></div>
+			<div class="alliance"></div>
+			
+</div><div class="userpic">
+
+	<!-- user image -->
+			<!-- simply remove the code before <img src="image name"> and after for the image to be displayed -->
+
+				<!--<img src="images/user/47.gif" />-->
+								
+</div><div class="user">
+
+	<!-- name and position -->
+	
+			<b>Username</b><br/>			Position
+
+	   
+</div></div>
+
+</div></td><td height="100%" valign="top"><div class="box2"><br>
+
+	<!-- general information -->
+			
+&nbsp;			Name: 				??				<br/>
+&nbsp;			Location: 			??				<br/>
+&nbsp;			Age: 				??				<br/><br/>
+
+
+&nbsp;			Information: 		??				</div></td></tr></table></div></div><br/>
+
+
+				<!-- End Staff Member --><!-- End Staff Member -->
+				<!-- End Staff Member --><!-- End Staff Member -->
+
+
+				<!-- Start Staff Member --><!-- Start Staff Member -->
+				<!-- Start Staff Member --><!-- Start Staff Member -->
+
+<div class="bg1"><div class="bg2"><table width="100%" height="100%" border="0" cellspacing="0" cellpadding="0"><tr><td width="140" height="100%" valign="top"><div class="box1"><div class="charbg">
+
+
+	<!-- character details -->
+			<!-- please refer to the guide to change character details -->
+	
+			<div class="human-male"></div>
+			<div class="warrior"></div>
+			<div class="alliance"></div>
+			
+</div><div class="userpic">
+
+	<!-- user image -->
+			<!-- simply remove the code before <img src="image name"> and after for the image to be displayed -->
+
+				<!--<img src="images/user/47.gif" />-->
+
+</div><div class="user">
+
+	<!-- name and position -->
+	
+			<b>Username</b><br/>			Position
+	   
+</div></div>
+
+</div></td><td height="100%" valign="top"><div class="box2"><br>
+
+	<!-- general information -->
+			
+&nbsp;			Name: 				??				<br/>
+&nbsp;			Location: 			??				<br/>
+&nbsp;			Age: 				??				<br/><br/>
+
+
+&nbsp;			Information: 		??				</div></td></tr></table></div></div><br/>
+
+
+				<!-- End Staff Member --><!-- End Staff Member -->
+				<!-- End Staff Member --><!-- End Staff Member -->
+
+</div></center></div><div class="story-bot2"></div>
+
+		<!-- End Game Masters Section --><!-- End Game Masters Section -->
+		<!-- End Game Masters Section --><!-- End Game Masters Section -->
+		<!-- End Game Masters Section --><!-- End Game Masters Section -->
+
+
+<br /></td></tr></table></div><div class="bottom"></div></div><div align="center" class="bot"><font class="style20"><br/></font><font class="style30">Copyright 2008 © <a href="http://wowps.org/forum">WoWps.org</a> and Yoursite.com. All rights reserved.<br />Designed and Coded by <a href="http://wowps.org/forum/member-kitten.html">Kitten</a> @ <a href="http://wowps.org/forum">WoWps.org</a></font></div></body></html>
