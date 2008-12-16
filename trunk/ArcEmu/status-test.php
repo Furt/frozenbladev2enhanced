ece7e17c72f7860582c14248955d4b1b899112a0
furt82 <furt82@e3bcf03a-c382-11dd-ae62-6dab0a2b1684>
2008-12-16 09:49:45 +0000

    removed stats files, going about it a different way

--- trunk/ArcEmu/status-test.php
+++ trunk/ArcEmu/status-test.php
@@ -29,7 +29,7 @@ require_once('./lib/config.php')
 
 <!-- script -->
 <center>
-<?php include('stats.xml') ?>
+<?php include('server_stats.xsl') ?>
 </center>
 
 </tr></table></div><div class="bottom"></div></div><div align="center" class="bot"><font class="style20"><br/></font><font class="style30">Copyright 2008 © <a href="http://wowps.org/forum">WoWps.org</a> and <?php  echo $config['Sitename']; ?>. All rights reserved.<br />Designed by <a href="http://wowps.org/forum/member-kitten.html">Kitten</a> and Coded by <a href="http://wowps.org/forum/member-furt.html">Furt</a> @ <a href="http://wowps.org/forum">WoWps.org</a></font></div></body></html>
