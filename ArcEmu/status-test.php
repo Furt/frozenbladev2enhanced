27c6f80a6af9b5a2777a181518432ab3e960e2cb
furt82 <furt82@e3bcf03a-c382-11dd-ae62-6dab0a2b1684>
2008-12-16 08:49:28 +0000

    Added: topnavi.php status-test.php - for testing the layout Modified: leftnavi.php - to include status-test.php

---
+++ ArcEmu/status-test.php
@@ -0,0 +1,33 @@
+// Project Name: FrozenBlade V2 Enhanced"
+// Date: 25.07.2008 inital version
+// Coded by: Furt
+// Developed by: Kitten
+// Email: *****
+// License: GNU General Public License (GPL)
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<?php
+require_once('./lib/config.php')
+?>
+<title><?php  echo $config['Title']; ?></title>
+
+<link rel="shortcut icon" href="images/favicon.ico">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<!--[if IE]><link href="css/ie-fix.css" rel="stylesheet" type="text/css"><![endif]-->
+<script type="text/javascript" src="./js/img-trans.js"></script>
+<script type="text/javascript" src="./js/pre-load.js"></script>
+</head><body>
+
+<div class="maintile"><div class="blue"><div class="gryphon-right"><div class="gryphon-left"></div></div></div></div><div class="wowlogo"></div><div></div><div class="container"><div class="top"></div>
+
+
+<div class="banner"></div>
+
+
+<div class="bar"><br /><?php include('./lib/topnavi.php') ?></div><div class="inner"><table align="center" width="718" border="0" cellspacing="1" cellpadding="1"><tr>
+
+<!-- script -->
+Test test
+
+</tr></table></div><div class="bottom"></div></div><div align="center" class="bot"><font class="style20"><br/></font><font class="style30">Copyright 2008 © <a href="http://wowps.org/forum">WoWps.org</a> and <?php  echo $config['Sitename']; ?>. All rights reserved.<br />Designed by <a href="http://wowps.org/forum/member-kitten.html">Kitten</a> and Coded by <a href="http://wowps.org/forum/member-furt.html">Furt</a> @ <a href="http://wowps.org/forum">WoWps.org</a></font></div></body></html>
