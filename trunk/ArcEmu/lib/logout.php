d67e78b4e58e9fdf385451820f1415d2291dbed8
furt82 <furt82@e3bcf03a-c382-11dd-ae62-6dab0a2b1684>
2008-12-17 10:21:13 +0000

    In case your wondering last few rev's are basicly a cleanup of the svn. Removed unused items. Added ChangeLog

--- trunk/ArcEmu/lib/logout.php
+++
@@ -1,9 +0,0 @@
-<?php 
-session_start();
-include('common-functions.php');
-session_unset();
-session_destroy();
-	// Change the page where redirect an user
-	// after logout.
-		redirect('../home.php');
-?>
