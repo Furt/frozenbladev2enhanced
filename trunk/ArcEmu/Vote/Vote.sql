8fd1189bff4270eb8176243b72fc1e1ca82de3fd
furt82 <furt82@e3bcf03a-c382-11dd-ae62-6dab0a2b1684>
2008-12-22 00:04:42 +0000

    small donate.php fix needed to move a div. home-test.php updated. Vote script started.

---
+++ trunk/ArcEmu/Vote/Vote.sql
@@ -0,0 +1,62 @@
+-- ----------------------------
+-- AVS Database
+-- ----------------------------
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`avs` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `avs`;
+
+SET FOREIGN_KEY_CHECKS=0;
+DROP TABLE IF EXISTS `realms`;
+CREATE TABLE `realms` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `name` varchar(32) default NULL,
+  `sqlhost` varchar(32) default NULL,
+  `sqluser` varchar(32) default NULL,
+  `sqlpass` varchar(32) default NULL,
+  `chardb` varchar(32) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `votemodules`;
+CREATE TABLE `votemodules` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `name` varchar(32) default NULL,
+  `image` varchar(128) default NULL,
+  `url` varchar(128) default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `voterewards`;
+CREATE TABLE `voterewards` (
+  `id` int(10) unsigned NOT NULL auto_increment,
+  `realm` tinyint(3) unsigned default NULL,
+  `name` varchar(32) default NULL,
+  `description` text,
+  `itemid` int(10) unsigned default NULL,
+  `points` tinyint(3) unsigned default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `votes`;
+CREATE TABLE `votes` (
+  `ip` varchar(16) default NULL,
+  `account` varchar(16) default NULL,
+  `module` tinyint(3) unsigned default NULL,
+  `time` int(11) default NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+INSERT INTO `realms` VALUES ('1', 'Server Name', 'localhost', 'root', 'ascent', 'logon');
+INSERT INTO `votemodules` VALUES ('1', 'Xtreme Top 100', 'http://reliable.uuuq.com/1/images/friends+vote/xtremetop.jpg', null);
+INSERT INTO `votemodules` VALUES ('2', 'WoW Server List', 'http://reliable.uuuq.com/1/images/friends+vote/wowservers.png', null);
+INSERT INTO `votemodules` VALUES ('3', 'Game Site 200', 'http://reliable.uuuq.com/1/images/friends+vote/game%20site%20200.JPG', null);
+INSERT INTO `votemodules` VALUES ('4', 'Gtop 100', 'http://reliable.uuuq.com/1/images/friends+vote/gtop100.JPG', null);
+
+-- ----------------------------
+-- Account Table Change
+-- ----------------------------
+
+USE 'logon';
+
+ALTER TABLE `accounts`
+ADD COLUMN `reward_points` mediumint(10) unsigned NOT NULL default '0';
--- trunk/ArcEmu/vote-test.php
+++ trunk/ArcEmu/vote-test.php
@@ -6,55 +6,40 @@
 * Email: *****
 * License: GNU General Public License (GPL)
   */ -->
-  
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml"><head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<?php session_start(); 
+<?php
 require_once('./lib/config.php')
 ?>
 <title><?php  echo $config['Title']; ?></title>
+
 <link rel="shortcut icon" href="images/favicon.ico">
 <link href="css/style.css" rel="stylesheet" type="text/css">
-<!--[if IE]><link href="css/ie-fix.css" rel="stylesheet" type="text/css"><![endif]-->
+<link href="css/ie-fix.css" rel="stylesheet" type="text/css">
+<script type="text/javascript" src=".js/shared.js"></script>
+<script type="text/javascript" src="./js/drag.js"></script>
+<script type="text/javascript" src="./js/ajax.js"></script>
 <script type="text/javascript" src="./js/img-trans.js"></script>
 <script type="text/javascript" src="./js/pre-load.js"></script>
-
 </head><body>
-<!-- vote -->
 
-<!--  vote -->
-<div class="maintile"><div class="blue"><div class="gryphon-right"><div class="gryphon-left"></div></div></div></div><div class="wowlogo"></div><div></div><div class="container">
-<div class="top"></div>
+<div class="maintile"><div class="blue"><div class="gryphon-right"><div class="gryphon-left"></div></div></div></div><div class="wowlogo"></div><div></div><div class="container"><div class="top"></div>
 
 
 <div class="banner"></div>
 
 
-<div class="bar"><br /><center></center></div><div class="inner"><table align="center" width="718" border="0" cellspacing="1" cellpadding="1"><tr>
+<div class="bar"><br /></div><div class="inner"><table align="center" width="718" border="0" cellspacing="1" cellpadding="1"><tr>
 <?php
 include('./lib/leftnavi.php')
 ?>
-<td width="430" valign="top"><div class="main"></div>
+<td width="430" valign="top">
 
-
-
-<!-- Start News Post -->
-<?php
-/*
-	This file is used to generate articles managed by the phpns system. 
-	Place this code wherever you want your articles displayed on your 
-	website. The page that this code is placed in should have a .php
-	extension.
-*/
-
-	$phpns['limit'] = '5';
-	include("news/shownews.php");
-?>
-<!-- End News Post -->
-
-
-
+  <!-- script start -->
+<?php include('./vote/index.php') ?>
+          <!-- script stop -->
+          
 <br /></td>
 <?php
 include('./lib/rightnavi.php')
