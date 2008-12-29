d1062ad460727f10d85484538b3f08d8c10e6853
furt82 <furt82@e3bcf03a-c382-11dd-ae62-6dab0a2b1684>
2008-12-29 15:59:31 +0000

    deathknight image added. more adds to status.php Will not be updating anymore untill i finish the arrayXML.class.php

--- trunk/ArcEmu/lib/arrayXML.class.php
+++ trunk/ArcEmu/lib/arrayXML.class.php
@@ -1,12 +1,4 @@
 <?php
-function convertGender($gender) {
-	if($gender == 1) {
-		$gender = "Female";
-		} else {
-			$gender = "Male";
-		}
-		return $gender;
-}
 function convertFaction($race) {
 	if($race == 2 || $race == 5 ||$race == 6 || $race == 8 || $race == 10) {
 		$race = '<img src="images/char/rank_default_1.gif" alt="Horde">';
@@ -14,5 +6,25 @@ function convertFaction($race) {
 			$race = '<img src="images/char/rank_default_0.gif" alt="Alliance">';
 		}
 		return $race;
+}
+function convertMap($map) {
+	switch($map) {
+		case 0:
+			$map = "Eastern Kingdoms";
+		break;
+		case 1:
+			$map = "Kalimdor";
+		break;
+		case 13:
+			$map = "Testing";
+		break;
+		case 25:
+			$map = "Scott Test";
+		break;
+		default: // if it is nothing of the above, this will be used
+			$map = "Unknown";
+		break;
+	}
+	return $map;
 }
 ?>
