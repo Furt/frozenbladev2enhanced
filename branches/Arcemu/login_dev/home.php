dcb13cb8c075fbf4797072824f40101d3a159d95
furt82 <furt82@e3bcf03a-c382-11dd-ae62-6dab0a2b1684>
2009-01-06 08:04:31 +0000

    ok one last small thing i forgot, the styles should be right now.

--- branches/Arcemu/login_dev/home.php
+++ branches/Arcemu/login_dev/home.php
@@ -129,11 +129,12 @@ include('./lib/rightnavi.php')
 <html xmlns="http://www.w3.org/1999/xhtml"><head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title><?php  echo $config['Title']; ?></title>
+
 <link rel="shortcut icon" href="images/favicon.ico">
-<link href="css/style.css" rel="stylesheet" type="text/css">
-<!--[if IE]><link href="css/ie-fix.css" rel="stylesheet" type="text/css"><![endif]-->
-<script type="text/javascript" src="./js/img-trans.js"></script>
-<script type="text/javascript" src="./js/pre-load.js"></script>
+<link href="style/style.css" rel="stylesheet" type="text/css">
+<!--[if IE]><link href="style/ie-fix.css" rel="stylesheet" type="text/css"><![endif]-->
+<script type="text/javascript" src="style/img-trans.js"></script>
+<script type="text/javascript" src="style/pre-load.js"></script>
 
 </head><body>
 
